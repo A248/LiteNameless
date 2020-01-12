327115ab0a1c801a6db6b0f4f827c3e2c5a2195e
A248 <anandebeh@gmail.com>
2020-01-12 17:02:52 -0500

    Update Bungee and Spigot implementations

--- litenameless-bungee/src/main/java/space/arim/nameless/bungee/LiteNamelessBungee.java
+++ litenameless-bungee/src/main/java/space/arim/nameless/bungee/LiteNamelessBungee.java
@@ -1,42 +1,41 @@
 /* 
- * LiteNameless-spigot
+ * LiteNameless-bungee
  * Copyright © 2020 Anand Beh <https://www.arim.space>
  * 
- * LiteNameless-spigot is free software: you can redistribute it and/or modify
+ * LiteNameless-bungee is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * LiteNameless-spigot is distributed in the hope that it will be useful,
+ * LiteNameless-bungee is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with LiteNameless-spigot. If not, see <https://www.gnu.org/licenses/>
+ * along with LiteNameless-bungee. If not, see <https://www.gnu.org/licenses/>
  * and navigate to version 3 of the GNU General Public License.
  */
-package space.arim.nameless.spigot;
+package space.arim.nameless.bungee;
 
 import java.util.UUID;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
-import space.arim.api.server.bukkit.SpigotUtil;
-
+import space.arim.api.server.bungee.BungeeUtil;
 import space.arim.namelessplugin.LiteNameless;
 import space.arim.namelessplugin.api.PlayerWrapper;
 import space.arim.namelessplugin.api.SenderWrapper;
 import space.arim.namelessplugin.api.ServerEnv;
 
-public class LiteNamelessSpigot extends JavaPlugin implements Listener, ServerEnv {
+public class LiteNamelessBungee extends Plugin implements Listener, ServerEnv {
 	
 	private LiteNameless core;
 	
@@ -44,6 +43,14 @@ public class LiteNamelessSpigot extends JavaPlugin implements Listener, ServerEn
 	public void onEnable() {
 		core = new LiteNameless(getLogger(), getDataFolder(), this);
 		core.reload();
+		getProxy().getPluginManager().registerCommand(this, new Command("litenameless") {
+			
+			@Override
+			public void execute(CommandSender sender, String[] args) {
+				core.executeCommand(new WrappedSender(sender), args);
+			}
+			
+		});
 	}
 	
 	@Override
@@ -52,20 +59,14 @@ public class LiteNamelessSpigot extends JavaPlugin implements Listener, ServerEn
 		core = null;
 	}
 	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		core.executeCommand(new WrappedSender(sender), args);
-		return true;
-	}
-	
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	private void onJoin(PlayerJoinEvent evt) {
+	@EventHandler(priority = EventPriority.HIGHEST)
+	private void onJoin(PostLoginEvent evt) {
 		core.login(new WrappedPlayer(evt.getPlayer()));
 	}
 
 	@Override
 	public PlayerWrapper getIfOnline(String name) {
-		for (Player player : getServer().getOnlinePlayers()) {
+		for (ProxiedPlayer player : getProxy().getPlayers()) {
 			if (player.getName().equalsIgnoreCase(name)) {
 				return new WrappedPlayer(player);
 			}
@@ -82,7 +83,7 @@ class WrappedSender implements SenderWrapper {
 	WrappedSender(CommandSender sender) {
 		this.sender = sender;
 	}
-	
+
 	@Override
 	public boolean hasPermission(String permission) {
 		return sender.hasPermission(permission);
@@ -90,25 +91,25 @@ class WrappedSender implements SenderWrapper {
 	
 	@Override
 	public void sendMessage(String message) {
-		sender.sendMessage(SpigotUtil.color(message));
+		sender.sendMessage(BungeeUtil.color(message));
 	}
 	
 }
 
 class WrappedPlayer extends WrappedSender implements PlayerWrapper {
 
-	WrappedPlayer(Player sender) {
+	WrappedPlayer(ProxiedPlayer sender) {
 		super(sender);
 	}
-
+	
 	@Override
 	public UUID getUniqueId() {
-		return ((Player) sender).getUniqueId();
+		return ((ProxiedPlayer) sender).getUniqueId();
 	}
 	
 	@Override
 	public String getName() {
-		return ((Player) sender).getName();
+		return ((ProxiedPlayer) sender).getName();
 	}
 	
 }
